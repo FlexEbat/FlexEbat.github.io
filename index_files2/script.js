89d27e1e8650b4cc2ada5c3e47162d6aea5b7e83
FlexEbat <52438328+FlexEbat@users.noreply.github.com>
2019-08-23 12:27:02 +0700

    Update commit

--- index_files2/script.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$('<link rel="stylesheet" href="../index_files/style.css">').appendTo('head');var i=0;function yved(){i=1;$('.yved:nth-child('+i+')').fadeIn(500).delay(5000).fadeOut(500);}setTimeout(function(){setInterval(function(){i=i+1;if(i>10)i=1;$('.yved:nth-child('+i+')').fadeIn(500).delay(5000).fadeOut(500);},14000);yved();},7000);});
